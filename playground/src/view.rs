930c0bb3a8bd72ff3e1df06b3f6f47ab5bf60eb7
Evgeniy Goncharuck <iterlace.dev@sent.com>
2022-05-30 19:17:52 +0300

    update/insert commands
    a very stupid, straightforward and non-scalable way
    but it took only 30min :)

--- playground/src/view.rs
+++ playground/src/view.rs
@@ -2,6 +2,9 @@ use std::io::{self, BufRead, Write};
 use regex::Regex;
 use versebase::table::TableSchema;
 use super::db::{Database};
+use super::schemas::*;
+use versebase::datatypes::{Int, Str, DateTime, DataType, DType};
+use versebase::datatypes;
 
 
 pub struct Playground {
@@ -101,12 +104,166 @@ impl Playground {
         }
     }
 
-    fn update(&self, command: Command) {
-        println!("update {:?}", command);
+    fn update(&mut self, command: Command) {
+        if command.arguments.len() < 3 {
+            println!("Usage: insert [artists, songs] [field1>; <field2>; ...]");
+            return;
+        }
+        let table = (&command.arguments[0]).to_string();
+
+        match table.as_str() {
+            "artists" => {
+                if command.arguments.len() != 3 {
+                    println!("artists table consists of exactly 2 fields. Check your input and try again.");
+                    return;
+                }
+                let id = match command.arguments[1].parse::<i32>() {
+                    Ok(val) => val,
+                    Err(_) => {
+                        println!("{} is not a valid id!", &command.arguments[1]);
+                        return;
+                    }
+                };
+                let name = command.arguments[2].clone();
+
+                if let Err(e) = self.db.artists.delete(id.clone()) {
+                    println!("Error: {}", e.message);
+                    return;
+                }
+
+                let artist = Artists {
+                    id: Int::new(id),
+                    name: Str::new(name),
+                };
+                match self.db.artists.create(artist) {
+                    Ok(_id) => println!("Updated artist with id = {}", _id),
+                    Err(e) => println!("Error: {}", e.message)
+                }
+            },
+            "songs" => {
+                if command.arguments.len() != 4 {
+                    println!("songs table consists of exactly 3 fields. Check your input and try again.");
+                    return;
+                }
+                let id = match command.arguments[1].parse::<i32>() {
+                    Ok(val) => val,
+                    Err(_) => {
+                        println!("{} is not a valid id!", &command.arguments[1]);
+                        return;
+                    }
+                };
+                let name = command.arguments[2].clone();
+                let artist_id = match command.arguments[3].parse::<i32>() {
+                    Ok(val) => val,
+                    Err(_) => {
+                        println!("{} is not a valid artist foreign key!", &command.arguments[1]);
+                        return;
+                    }
+                };
+
+                // Check if given artist exists
+                match self.db.artists.get(artist_id.clone()) {
+                    Ok(_) => {},
+                    Err(e) => {println!("Error: {}", e.message); return}
+                }
+
+
+                if let Err(e) = self.db.songs.delete(id.clone()) {
+                    println!("Error: {}", e.message);
+                    return;
+                }
+
+
+                let song = Songs {
+                    id: Int::new(id),
+                    name: Str::new(name),
+                    artist_id: Int::new(artist_id)
+                };
+                match self.db.songs.create(song) {
+                    Ok(_id) => println!("Updated song with id = {}", _id),
+                    Err(e) => println!("Error: {}", e.message)
+                }
+            },
+            table_name => {
+                println!("Table \"{}\" is not supported", table_name);
+                return;
+            }
+        }
     }
 
-    fn insert(&self, command: Command) {
-        println!("insert {:?}", command);
+    fn insert(&mut self, command: Command) {
+        if command.arguments.len() < 3 {
+            println!("Usage: insert [artists, songs] [field1>; <field2>; ...]");
+            return;
+        }
+        let table = (&command.arguments[0]).to_string();
+
+        match table.as_str() {
+            "artists" => {
+                if command.arguments.len() != 3 {
+                    println!("artists table consists of exactly 2 fields. Check your input and try again.");
+                    return;
+                }
+                let id = match command.arguments[1].parse::<i32>() {
+                    Ok(val) => val,
+                    Err(_) => {
+                        println!("{} is not a valid id!", &command.arguments[1]);
+                        return;
+                    }
+                };
+                let name = command.arguments[2].clone();
+
+                let artist = Artists {
+                    id: Int::new(id),
+                    name: Str::new(name),
+                };
+                match self.db.artists.create(artist) {
+                    Ok(_id) => println!("Created artist with id = {}", _id),
+                    Err(e) => println!("Error: {}", e.message)
+                }
+            },
+            "songs" => {
+                if command.arguments.len() != 4 {
+                    println!("songs table consists of exactly 3 fields. Check your input and try again.");
+                    return;
+                }
+                let id = match command.arguments[1].parse::<i32>() {
+                    Ok(val) => val,
+                    Err(_) => {
+                        println!("{} is not a valid id!", &command.arguments[1]);
+                        return;
+                    }
+                };
+                let name = command.arguments[2].clone();
+                let artist_id = match command.arguments[3].parse::<i32>() {
+                    Ok(val) => val,
+                    Err(_) => {
+                        println!("{} is not a valid foreign key!", &command.arguments[1]);
+                        return;
+                    }
+                };
+
+                // Check if given artist exists
+                match self.db.artists.get(artist_id.clone()) {
+                    Ok(_) => {},
+                    Err(e) => {println!("Error: {}", e.message); return}
+                }
+
+                let song = Songs {
+                    id: Int::new(id),
+                    name: Str::new(name),
+                    artist_id: Int::new(artist_id)
+                };
+                match self.db.songs.create(song) {
+                    Ok(_id) => println!("Created song with id = {}", _id),
+                    Err(e) => println!("Error: {}", e.message)
+                }
+            },
+            table_name => {
+                println!("Table \"{}\" is not supported", table_name);
+                return;
+            }
+        }
     }
 
     fn delete(&mut self, command: Command) {
@@ -144,8 +301,8 @@ impl Playground {
     fn help(&self) {
         println!("List of available commands:\n\
                     \tget [artists, songs] <id>\n\
-                    \tupdate [artists, songs] <id>\n\
-                    \tinsert [artists, songs] <id>\n\
+                    \tupdate [artists, songs] <id> [field1>; <field2>; ...]\n\
+                    \tinsert [artists, songs] [field1>; <field2>; ...]\n\
                     \tdelete [artists, songs] <id>\n\
                     \thelp\n\
                     \texit\
